891cb3d609de7e31160ee64af60b915a40bcd456
Lain MultipleInstances <zlainsama@163.com>
2019-01-15 21:32:41 +0800

    changes

--- src/main/java/lain/mods/peacefulsurface/init/fabric/mixins/FabricPeacefulSurfaceMixin.java
+++ src/main/java/lain/mods/peacefulsurface/init/fabric/mixins/FabricPeacefulSurfaceMixin.java
@@ -1,10 +1,11 @@
-package lain.mods.peacefulsurface.init.fabric;
+package lain.mods.peacefulsurface.init.fabric.mixins;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import lain.mods.peacefulsurface.api.PeaceAPI;
+import lain.mods.peacefulsurface.init.fabric.FabricPeacefulSurface;
 import net.minecraft.entity.EntityType;
 import net.minecraft.sortme.SpawnHelper;
 import net.minecraft.sortme.SpawnRestriction;
@@ -15,8 +16,8 @@ import net.minecraft.world.ViewableWorld;
 public class FabricPeacefulSurfaceMixin
 {
 
-    @Inject(method = "canSpawn(Lnet/minecraft/sortme/SpawnRestriction$Location;Lnet/minecraft/world/ViewableWorld;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/EntityType;)Z", at = @At("RETURN"), cancellable = true, require = 1)
-    private void onCanSpawn_nBXjeY(SpawnRestriction.Location location, ViewableWorld world, BlockPos pos, EntityType<?> entitytype, CallbackInfoReturnable<Boolean> info)
+    @Inject(method = "canSpawn(Lnet/minecraft/sortme/SpawnRestriction$Location;Lnet/minecraft/world/ViewableWorld;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/EntityType;)Z", at = @At("RETURN"), cancellable = true)
+    private static void onCanSpawn_nBXjeY(SpawnRestriction.Location location, ViewableWorld world, BlockPos pos, EntityType<?> entitytype, CallbackInfoReturnable<Boolean> info)
     {
         if (info.getReturnValue() && PeaceAPI.filterEntity(FabricPeacefulSurface.wrapEntity(entitytype), FabricPeacefulSurface.wrapWorld(world), pos.getX(), pos.getY(), pos.getZ()))
             info.setReturnValue(false);
