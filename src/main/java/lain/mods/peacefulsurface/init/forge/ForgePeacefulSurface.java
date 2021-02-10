fb88a6b0fb9da36b405f1e9c93f4f211e45c19f6
Lain MultipleInstances <zlainsama@163.com>
2021-02-10 20:13:00 +0800

    register DISPLAYTEST as server-only

--- src/main/java/lain/mods/peacefulsurface/init/forge/ForgePeacefulSurface.java
+++ src/main/java/lain/mods/peacefulsurface/init/forge/ForgePeacefulSurface.java
@@ -1,13 +1,19 @@
 package lain.mods.peacefulsurface.init.forge;
 
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Mod("peacefulsurface")
 public class ForgePeacefulSurface {
 
     public ForgePeacefulSurface() {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (v, n) -> true));
+
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
     }
 
