d813179f42224c6c959844b0defcd026d829a55d
Lain MultipleInstances <zlainsama@163.com>
2023-06-09 15:45:24 +0800

    Remove redundant code

--- src/main/java/lain/mods/peacefulsurface/init/forge/ForgePeacefulSurface.java
+++ src/main/java/lain/mods/peacefulsurface/init/forge/ForgePeacefulSurface.java
@@ -1,18 +1,15 @@
 package lain.mods.peacefulsurface.init.forge;
 
-import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.network.NetworkConstants;
 import org.apache.logging.log4j.Logger;
 
 @Mod("peacefulsurface")
 public class ForgePeacefulSurface {
 
     public ForgePeacefulSurface() {
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (v, n) -> true));
+//        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (v, n) -> true));
 
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
     }
