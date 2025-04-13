1bd38f7535c4f371c401c17fa72ac02a4055c37e
Jon Zimbel <jzimbel@users.noreply.github.com>
2025-04-13 14:36:43 -0400

    update to 1.21.5 fabric

--- src/main/java/lain/mods/peacefulsurface/init/fabric/mixins/FabricPeacefulSurfaceMixin.java
+++ src/main/java/lain/mods/peacefulsurface/init/fabric/mixins/FabricPeacefulSurfaceMixin.java
@@ -19,7 +19,7 @@ public abstract class FabricPeacefulSurfaceMixin {
 
     @Inject(method = "canSpawn(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/SpawnGroup;Lnet/minecraft/world/gen/StructureAccessor;Lnet/minecraft/world/gen/chunk/ChunkGenerator;Lnet/minecraft/world/biome/SpawnSettings$SpawnEntry;Lnet/minecraft/util/math/BlockPos$Mutable;D)Z", at = @At("RETURN"), cancellable = true)
     private static void onCanSpawn_nBXjeY(ServerWorld world, SpawnGroup group, StructureAccessor structureAccessor, ChunkGenerator chunkGenerator, SpawnSettings.SpawnEntry spawnEntry, BlockPos.Mutable pos, double squaredDistance, CallbackInfoReturnable<Boolean> info) {
-        if (info.getReturnValue() && PeaceAPI.filterEntity(FabricPeacefulSurface.wrapEntity(spawnEntry.type), FabricPeacefulSurface.wrapWorld(world), pos.getX(), pos.getY(), pos.getZ()))
+        if (info.getReturnValue() && PeaceAPI.filterEntity(FabricPeacefulSurface.wrapEntity(spawnEntry.type()), FabricPeacefulSurface.wrapWorld(world), pos.getX(), pos.getY(), pos.getZ()))
             info.setReturnValue(false);
     }
 
